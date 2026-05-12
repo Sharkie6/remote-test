cd88f9a09c83b5fad7962f3e8d4b4edd647e4dbb
Sharkie6 <2746667238@qq.com>
2026-05-12 16:08:12 +0800

    pdfloader进行了更改

--- pdfloader使用.py
+++ pdfloader使用.py
@@ -1,12 +1,4 @@
-from langchain_community.document_loaders import PyPDFLoader
-
-loader = PyPDFLoader("./data/pdf2.pdf",mode="page",password="itheima")
-
-i=0
-for doc in loader.load():
-    print(doc)
-    i+=1
-    print("="*20,"第",i,"页",sep="")
+89
 
 
 
