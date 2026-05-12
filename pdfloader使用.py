14a7e48cc44b0e9f8014d0b84176ae9fc0432a6f
Sharkie6 <2746667238@qq.com>
2026-05-12 16:21:06 +0800

    新加规则文件

--- pdfloader使用.py
+++ pdfloader使用.py
@@ -1,4 +1,12 @@
-89
+from langchain_community.document_loaders import PyPDFLoader
+
+loader = PyPDFLoader("./data/pdf2.pdf",mode="page",password="itheima")
+
+i=0
+for doc in loader.load():
+    print(doc)
+    i+=1
+    print("="*20,"第",i,"页",sep="")
 
 
 
